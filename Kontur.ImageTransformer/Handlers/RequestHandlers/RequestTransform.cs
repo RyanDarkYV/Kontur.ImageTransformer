64e3c9d2e83e279e65ec20a89cf988cd5c31bbd4
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-07-02 15:50:08 +0300

    Add RequestTransform(Dto for transform operation and coords)

---
+++ Kontur.ImageTransformer/Handlers/RequestHandlers/RequestTransform.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Kontur.ImageTransformer.Handler
+{
+    internal class RequestTransform
+    {
+        public RequestTransform(RotateFlipType operation, Rectangle rectangle)
+        {
+            Operation = operation;
+            Rectangle = rectangle;
+        }
+
+        public RotateFlipType Operation { get; }
+        public Rectangle Rectangle { get; }
+
+    }
+}
